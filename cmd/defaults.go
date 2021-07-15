5c54678713afc505cd6db06db2aa2f437e441687
Dan Esparza <esparza.dan@gmail.com>
2021-07-15 12:41:49 -0400

    Initial commit, including CI and debian package files

---
+++ cmd/defaults.go
@@ -0,0 +1,43 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	jsonConfig bool
+	yamlConfig bool
+)
+
+var yamlDefault = []byte(`
+loglevel: INFO
+`)
+
+var jsonDefault = []byte(`{	
+	"loglevel": "INFO"
+}`)
+
+// defaultsCmd represents the defaults command
+var defaultsCmd = &cobra.Command{
+	Use:   "defaults",
+	Short: "Prints default fxdmx configuration file",
+	Long: `Use this to create a default configuration file for fxdmx. 
+	Example:
+	fxdmx defaults > fxdmx.yaml`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if jsonConfig {
+			fmt.Printf("%s", jsonDefault)
+		} else if yamlConfig {
+			fmt.Printf("%s", yamlDefault)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(defaultsCmd)
+
+	defaultsCmd.Flags().BoolVarP(&jsonConfig, "json", "j", false, "Create a JSON configuration file")
+	defaultsCmd.Flags().BoolVarP(&yamlConfig, "yaml", "y", true, "Create a YAML configuration file")
+}
---
+++ cmd/version.go
@@ -0,0 +1,35 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// BuildVersion is the app version number at build time
+var BuildVersion = "Unknown"
+
+// CommitID is the SHA commit for the compiled app at build time
+var CommitID string
+
+// versionCmd represents the version command
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Shows the version information",
+	Run: func(cmd *cobra.Command, args []string) {
+		//	Show the version number
+		fmt.Printf("\nfxdmx version %s", BuildVersion)
+
+		//	Show the CommitID if available:
+		if CommitID != "" {
+			fmt.Printf(" (%s)", CommitID[:7])
+		}
+
+		//	Trailing space and newline
+		fmt.Println(" ")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
