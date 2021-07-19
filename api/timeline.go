f7f4ab499db5de77c495bf4a6bd8bcdc1b5b6ef1
Dan Esparza <esparza.dan@gmail.com>
2021-07-19 12:41:17 -0400

    Add CreateTimeline api method

--- api/timeline.go
+++ api/timeline.go
@@ -4,12 +4,14 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/danesparza/fxdmx/event"
 )
 
 // ListAllTimelines godoc
 // @Summary List all timelines in the system
 // @Description List all timelines in the system
-// @Tags triggers
+// @Tags timelines
 // @Accept  json
 // @Produce  json
 // @Success 200 {object} api.SystemResponse
@@ -29,6 +31,57 @@ func (service Service) ListAllTimelines(rw http.ResponseWriter, req *http.Reques
 	response := SystemResponse{
 		Message: fmt.Sprintf("%v timeline(s)", len(retval)),
 		Data:    retval,
+	}
+
+	//	Serialize to JSON & return the response:
+	rw.Header().Set("Content-Type", "application/json; charset=utf-8")
+	json.NewEncoder(rw).Encode(response)
+}
+
+// CreateTimeline godoc
+// @Summary Create a new timeline
+// @Description Create a new timeline
+// @Tags timelines
+// @Accept  json
+// @Produce  json
+// @Param timeline body api.CreateTimelineRequest true "The timeline to create"
+// @Success 200 {object} api.SystemResponse
+// @Failure 400 {object} api.ErrorResponse
+// @Failure 500 {object} api.ErrorResponse
+// @Router /timelines [post]
+func (service Service) CreateTimeline(rw http.ResponseWriter, req *http.Request) {
+
+	//	req.Body is a ReadCloser -- we need to remember to close it:
+	defer req.Body.Close()
+
+	//	Decode the request
+	request := CreateTimelineRequest{}
+	err := json.NewDecoder(req.Body).Decode(&request)
+	if err != nil {
+		sendErrorResponse(rw, err, http.StatusBadRequest)
+		return
+	}
+
+	//	If we don't have any webhooks associated, make sure we indicate that's not valid
+	if len(request.Frames) < 1 {
+		sendErrorResponse(rw, fmt.Errorf("at least one frame must be included"), http.StatusBadRequest)
+		return
+	}
+
+	//	Create the new timeline:
+	newTimeline, err := service.DB.AddTimeline(request.Name, request.Frames)
+	if err != nil {
+		sendErrorResponse(rw, err, http.StatusInternalServerError)
+		return
+	}
+
+	//	Record the event:
+	service.DB.AddEvent(event.TimelineCreated, fmt.Sprintf("%+v", request), GetIP(req), service.HistoryTTL)
+
+	//	Create our response and send information back:
+	response := SystemResponse{
+		Message: "Timeline created",
+		Data:    newTimeline,
 	}
 
 	//	Serialize to JSON & return the response:
