56365c464773fbc591af0336ce6149b31ffee327
Dan Esparza <esparza.dan@gmail.com>
2021-07-26 10:16:13 -0400

    Add code to process the timeline play request

--- dmx/process.go
+++ dmx/process.go
@@ -4,15 +4,23 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"strings"
 	"sync"
 	"time"
 
+	"github.com/akualab/dmx"
 	"github.com/danesparza/fxdmx/data"
+	"github.com/danesparza/fxdmx/event"
 )
 
 type PlayTimelineRequest struct {
 	ProcessID         string
 	RequestedTimeline data.Timeline
+}
+
+type timelineProcessMap struct {
+	m       map[string]func()
+	rwMutex sync.RWMutex
 }
 
 // BackgroundProcess encapsulates background processing operations
@@ -28,18 +36,16 @@ type BackgroundProcess struct {
 
 	// StopAllTimelines signals all running timlines should be stopped
 	StopAllTimelines chan bool
-}
 
-type timelineProcessMap struct {
-	m       map[string]func()
-	rwMutex sync.RWMutex
+	// PlayingTimelines tracks currently playing timelines
+	PlayingTimelines timelineProcessMap
 }
 
 // HandleAndProcess handles system context calls and channel events to play/stop audio
-func (bp BackgroundProcess) HandleAndProcess(systemctx context.Context) {
+func (bp *BackgroundProcess) HandleAndProcess(systemctx context.Context) {
 
 	//	Create a map of running timelines and their cancel functions
-	playingTimelines := timelineProcessMap{m: make(map[string]func())}
+	bp.PlayingTimelines.m = make(map[string]func())
 
 	//	Loop and respond to channels:
 	for {
@@ -47,35 +53,13 @@ func (bp BackgroundProcess) HandleAndProcess(systemctx context.Context) {
 		case playReq := <-bp.PlayTimeline:
 			//	As we get a request on a channel to play a file...
 			//	Spawn a goroutine
-			go func(cx context.Context, req PlayTimelineRequest) {
-
-				//	Create a cancelable context from the passed (system) context
-				_, cancel := context.WithCancel(cx)
-				defer cancel()
-
-				//	Add an entry to the map with
-				//	- key: instance id
-				//	- value: the cancel function (pointer)
-				//	(critical section)
-				playingTimelines.rwMutex.Lock()
-				playingTimelines.m[req.ProcessID] = cancel
-				playingTimelines.rwMutex.Unlock()
-
-				//	Process the timeline
-				log.Printf("Processing timeline %v\n", req.ProcessID)
-
-				//	Remove ourselves from the map and exit (critical section)
-				playingTimelines.rwMutex.Lock()
-				delete(playingTimelines.m, req.ProcessID)
-				playingTimelines.rwMutex.Unlock()
-
-			}(systemctx, playReq) // Launch the goroutine
+			go bp.StartTimelinePlay(systemctx, playReq) // Launch the goroutine
 
 		case stopTL := <-bp.StopTimeline:
 
 			//	Look up the item in the map and call cancel if the item exists (critical section):
-			playingTimelines.rwMutex.Lock()
-			playCancel, exists := playingTimelines.m[stopTL]
+			bp.PlayingTimelines.rwMutex.Lock()
+			playCancel, exists := bp.PlayingTimelines.m[stopTL]
 
 			if exists {
 				//	Call the context cancellation function
@@ -84,18 +68,18 @@ func (bp BackgroundProcess) HandleAndProcess(systemctx context.Context) {
 				log.Printf("Stopped timeline process %v\n", stopTL)
 
 				//	Remove ourselves from the map and exit
-				delete(playingTimelines.m, stopTL)
+				delete(bp.PlayingTimelines.m, stopTL)
 			}
-			playingTimelines.rwMutex.Unlock()
+			bp.PlayingTimelines.rwMutex.Unlock()
 
 		case <-bp.StopAllTimelines:
 
 			//	Loop through all items in the map and call cancel if the item exists (critical section):
-			playingTimelines.rwMutex.Lock()
+			bp.PlayingTimelines.rwMutex.Lock()
 
 			log.Printf("Stopping all timeline processes\n")
 
-			for stopTL, playCancel := range playingTimelines.m {
+			for stopTL, playCancel := range bp.PlayingTimelines.m {
 
 				//	Call the cancel function
 				playCancel()
@@ -103,14 +87,158 @@ func (bp BackgroundProcess) HandleAndProcess(systemctx context.Context) {
 				//	Remove ourselves from the map
 				//	(this is safe to do in a 'range':
 				//	https://golang.org/doc/effective_go#for )
-				delete(playingTimelines.m, stopTL)
+				delete(bp.PlayingTimelines.m, stopTL)
 			}
 
-			playingTimelines.rwMutex.Unlock()
+			bp.PlayingTimelines.rwMutex.Unlock()
 
 		case <-systemctx.Done():
 			fmt.Println("Stopping timeline processor")
 			return
 		}
 	}
+}
+
+// PlayTimeline plays a timeline
+func (bp *BackgroundProcess) StartTimelinePlay(cx context.Context, req PlayTimelineRequest) {
+	//	Create a cancelable context from the passed context
+	ctx, cancel := context.WithCancel(cx)
+	defer cancel()
+
+	//	Add an entry to the map with
+	//	- key: instance id
+	//	- value: the cancel function (pointer)
+	//	(critical section)
+	bp.PlayingTimelines.rwMutex.Lock()
+	bp.PlayingTimelines.m[req.ProcessID] = cancel
+	bp.PlayingTimelines.rwMutex.Unlock()
+
+	//	Process the timeline
+	log.Printf("Processing timeline %v\n", req.ProcessID)
+
+	//	First, see if the timeline has a device set on it.
+	if strings.TrimSpace(req.RequestedTimeline.USBDevicePath) == "" {
+		defaultDevice, err := bp.DB.GetDefaultUSBDev()
+		if err != nil {
+			bp.DB.AddEvent(event.TimelineError, fmt.Sprintf("An error occurred trying to get the default USB device: %v", err), "", bp.HistoryTTL)
+			return
+		}
+
+		//	If it doesn't, grab the default and use that.
+		req.RequestedTimeline.USBDevicePath = defaultDevice
+	}
+
+	// Connect to the DMX controller.
+	dmx, e := dmx.NewDMXConnection(req.RequestedTimeline.USBDevicePath)
+	if e != nil {
+		log.Printf("ERROR: Unable to connect to DMX512 interface %v: %v", req.RequestedTimeline.USBDevicePath, e)
+		return
+	}
+	defer dmx.Close()
+
+	//	Keep a channel state map:
+	channelState := map[int]byte{}
+
+	//	Our waitgroup (for sync'ing fade finishes)
+	var wg sync.WaitGroup
+
+	//	Iterate through each frame
+	for _, frame := range req.RequestedTimeline.Frames {
+
+		select {
+		default:
+
+			//	Find out what type of frame this is, and act accordingly:
+			switch strings.ToLower(frame.Type) {
+			case "scene":
+				//	Iterate through each of the channels and set them, then render
+				for _, channel := range frame.Channels {
+					//	Set dmx value for each channel:
+					dmx.SetChannel(channel.Channel, channel.Value)
+
+					//	Track chennel state:
+					channelState[channel.Channel] = channel.Value
+				}
+				dmx.Render()
+
+			case "fade":
+
+				//	Iterate through each of the channels.
+				for _, channel := range frame.Channels {
+
+					wg.Add(1)
+
+					//	Find the initial value, and pass that to the fade operation:
+					//	(if we can't find it, assume it's 0 and pass that)
+					ivalue, prs := channelState[channel.Channel]
+					if !prs {
+						ivalue = 0
+					}
+
+					go func(channelInfo data.ChannelValue, initialValue byte) {
+						// Decrement the counter when the goroutine completes.
+						defer wg.Done()
+
+						//	Compare with the initial state, then render repeatedly
+						//	toward the target value in a for loop (pay attention to the direction).
+						if initialValue < channelInfo.Value {
+							//	We need to fade up
+							for i := initialValue; i < channelInfo.Value; i++ {
+								select {
+								case <-time.After(1 * time.Millisecond):
+									dmx.SetChannel(channelInfo.Channel, byte(i))
+									dmx.Render()
+								case <-ctx.Done():
+									log.Println("Stopping from fadeup!")
+									return
+								}
+							}
+						} else {
+							//	We need to fade down
+							for i := initialValue; i > 0; i-- {
+								select {
+								case <-time.After(1 * time.Millisecond):
+									dmx.SetChannel(channelInfo.Channel, byte(i))
+									dmx.Render()
+								case <-ctx.Done():
+									log.Println("Stopping from fadedown!")
+									return
+								}
+							}
+						}
+
+					}(channel, ivalue)
+
+					//	This means we'll need to put a mutex around them map. (for thread safe map interactions)
+
+					//	Also:  Track the new value of the channel
+					channelState[channel.Channel] = channel.Value
+
+				}
+
+				// Wait for all fades to complete.
+				wg.Wait()
+
+			case "sleep":
+				//	Just sleep for the specified number of seconds
+				select {
+				case <-time.After(time.Duration(frame.SleepTime) * time.Second):
+					continue
+				case <-ctx.Done():
+					log.Println("Stopping from sleep!")
+					return
+				}
+			}
+
+		case <-ctx.Done():
+			// stop
+			log.Println("Stopping in main loop!")
+			return
+		}
+	}
+
+	//	Remove ourselves from the map and exit (critical section)
+	bp.PlayingTimelines.rwMutex.Lock()
+	delete(bp.PlayingTimelines.m, req.ProcessID)
+	bp.PlayingTimelines.rwMutex.Unlock()
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/danesparza/fxdmx
 go 1.16
 
 require (
+	github.com/akualab/dmx v0.0.0-20130922234952-1ec6837faba7 // indirect
 	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/hashicorp/logutils v1.0.0 // indirect
@@ -17,6 +18,7 @@ require (
 	github.com/spf13/viper v1.8.1
 	github.com/swaggo/http-swagger v1.0.0 // indirect
 	github.com/swaggo/swag v1.7.0 // indirect
+	github.com/tarm/goserial v0.0.0-20151007205400-b3440c3c6355 // indirect
 	github.com/tidwall/buntdb v1.2.4
 	golang.org/x/text v0.3.6 // indirect
 	gopkg.in/airbrake/gobrake.v2 v2.0.9 // indirect
